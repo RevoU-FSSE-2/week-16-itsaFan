353e03086497d0b1fe9d0cba546ac6feaa1abad6
itsaFan <steffansim@gmail.com>
2023-10-12 06:25:06 +0800

    add new middleware

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,5 +1,9 @@
 const { applyCors } = require("./cors-config");
+const { LoginLimiter } = require("./limiter");
+const { generateResetToken } = require("./uid");
 
 module.exports = {
   applyCors,
+  LoginLimiter,
+  generateResetToken,
 };
