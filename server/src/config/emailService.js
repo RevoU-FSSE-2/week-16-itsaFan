669d14afc86ac680fe295cc0008fb89e5c150a1b
itsaFan <steffansim@gmail.com>
2023-10-12 06:31:52 +0800

    add fake SMTP service

---
+++ server/src/config/emailService.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "graham.ebert@ethereal.email",
+    pass: "cCA53ZfG8jhkWBpdnh",
+  },
+});
+
+const sendEmail = async (options) => {
+  const mailOptions = {
+    from: "graham.ebert@ethereal.email",
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendEmail };
